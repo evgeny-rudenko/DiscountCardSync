07633fe57b151d7428e653d9ddbbc4ef09df4eeb
Evgeny Rudenko <rudenkoevgeny@gmail.com>
2022-09-03 01:37:13 +0300

    1

--- MysqlCard.cs
+++ MysqlCard.cs
@@ -19,10 +19,7 @@ namespace DiscountCardSync
             string MysqlConnectionString = File.ReadAllText("mysql.txt"); 
             using (MySqlConnection cnn = new MySqlConnection(MysqlConnectionString))
             {
-                // connection string sample
-                // Server = myServerAddress; Port = 1234; Database = myDataBase; Uid = myUsername; Pwd = myPassword;
-                // Efarma2MustDie
-                // Server = 192.168.1.82; Port = 3307; Database = backoffice; Uid = root; Pwd = Efarma2MustDie;
+             
                 mysqlCards = cnn.Query<MysqlCardMembers>("select * from discount").ToList ();
 
             }
